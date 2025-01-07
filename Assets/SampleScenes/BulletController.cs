eb45cd881e01068dbde798925b508d7c9b3dcf3f
RUI-QIU <scygz2@nottingham.edu.cn>
2025-01-07 18:23:01 +0800

    Add Data preprocess part1

---
+++ Assets/SampleScenes/BulletController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class BulletController: MonoBehaviour
+{
+    // 子弹存活时间（秒）
+    public float lifeTime = 10f;
+
+    private void Start()
+    {
+        // 在指定时间后自动销毁子弹
+        Destroy(gameObject, lifeTime);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        // 如果碰撞到目标小球，销毁自己
+        if (collision.gameObject.CompareTag("BlueBall"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/SampleScenes/Device1Setup.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Device1Setup : MonoBehaviour
+{
+    public GameObject devicepart2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        devicepart2.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        // 如果碰撞到目标小球，销毁自己
+        if (collision.gameObject.CompareTag("DevicePart1"))
+        {
+            devicepart2.SetActive(true);
+        }
+    }
+}
